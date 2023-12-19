bc78861a582ed9ccd1b79635cb15c97a7219b891
kuhhhhhhh <touruuu@gmail.com>
2023-12-19 09:57:00 +0800

    week15

---
+++ week15/week15-1a.py
@@ -0,0 +1,12 @@
+#原題目
+nums = [4,2,5,9,7,4,8]
+print(nums)
+#把題目照順序排出來
+nums.sort()
+print(nums)
+#找出最小兩個
+print( nums[0], nums[1] ) 
+#找出最大兩個
+N = len(nums)
+print('長度是', N)
+print( nums[N-1], nums[N-2] ) 
